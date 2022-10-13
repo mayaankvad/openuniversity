1077e05fde5367840b4c9deddb83f76444e989d1
Goce Partaloski <goce.partaloski@gmail.com>
2022-10-13 18:40:06 -0400

    First Commit

--- hello.py
+++ hello.py
@@ -1,14 +1,3 @@
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+print("Hello Capstone!!!")
 
-print("Hello World #2 !")
-
-print("Hello World From feature/one !")
-print("Hello World From feature/one #1 !")
-print("Hello World From feature/one #2 !")
-
-
-print("second stash")
-print("my new feature")
 
