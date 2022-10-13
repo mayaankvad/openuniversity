257e14a3c254675686add14aed1961386896afd2
Milton Casiano <90805282+MiltonCasiano88@users.noreply.github.com>
2022-10-13 18:44:04 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,3 +12,9 @@ print("Hello World From feature/one #2 !")
 print("second stash")
 print("my new feature")
 
+print("Hello World of GitHub!")
+print("Wasssssssssss up")
+
+value = 5
+
+print(f"{value} multiplied by 2 is: {value * 2}")
